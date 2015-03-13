9df0efcaac780c9006a9515fe3c6545d60c377c9
neilhanekom <neilhanekom1@gmail.com>
2015-03-13 09:25:12 +0200

    firt commit

--- config/assets/production.js
+++ config/assets/production.js
@@ -4,30 +4,27 @@ module.exports = {
 	client: {
 		lib: {
 			css: [
-				// 'public/lib/bootstrap/dist/css/bootstrap.css',
-				// 'public/lib/bootstrap/dist/css/bootstrap-theme.css',
-				'public/lib/angular-material/angular-material.css',
-				'public/lib/font-awesome/css/font-awesome.css',
+				'public/lib/angular-material/angular-material.min.css',
+				'public/lib/font-awesome/css/font-awesome.min.css',
 				'public/lib/font-awesome/fonts/fontawesome-webfont.svg',
-				'public/lib/v-accordion/dist/v-accordion.css',
-				'public/lib/animate.css/animate.css'
+				'public/lib/v-accordion/dist/v-accordion.min.css',
+				'public/lib/animate.css/animate.min.css'
 			],
 			js: [
-				'public/lib/angular/angular.js',
-				'public/lib/angular-resource/angular-resource.js',
-				'public/lib/angular-aria/angular-aria.js',
-				'public/lib/angular-animate/angular-animate.js',
-				'public/lib/angular-ui-router/release/angular-ui-router.js',
-				'public/lib/angular-ui-utils/ui-utils.js',
-				'public/lib/angular-bootstrap/ui-bootstrap-tpls.js',
-				'public/lib/angular-file-upload/angular-file-upload.js',
-				'public/lib/angular-material/angular-material.js',
+				'public/lib/angular/angular.min.js',
+				'public/lib/angular-resource/angular-resource.min.js',
+				'public/lib/angular-aria/angular-aria.min.js',
+				'public/lib/angular-animate/angular-animate.min.js',
+				'public/lib/angular-ui-router/release/angular-ui-router.min.js',
+				'public/lib/angular-ui-utils/ui-utils.min.js',
+				'public/lib/angular-file-upload/angular-file-upload.min.js',
+				'public/lib/angular-material/angular-material.min.js',
 				'public/lib/angular-svg-round-progressbar/build/roundProgress.min.js',
-				'public/lib/v-accordion/dist/v-accordion.js',
-				'public/lib/lodash/dist/lodash.js',
-				'public/lib/angular-google-maps/dist/angular-google-maps.js',
-				'public/lib/angular-material-icons/angular-material-icons.js',
-				'public/lib/angular-messages/angular-messages.js'
+				'public/lib/v-accordion/dist/v-accordion.min.js',
+				'public/lib/lodash/dist/lodash.min.js',
+				'public/lib/angular-google-maps/dist/angular-google-maps.min.js',
+				'public/lib/angular-material-icons/angular-material-icons.min.js',
+				'public/lib/angular-messages/angular-messages.min.js'
 			]
 		},
 		css: 'public/dist/application.min.css',
